de101725f807b506be08bbf488c64829763b8045
Raul Andrade <theandraderaul@gmail.com>
2018-12-05 03:11:31 +0000

    adicionando comentarios

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -10,17 +10,17 @@ public class Emissor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    factory.setUri("amqp://mtvezqic:oT0QoH-plEwlIWdYPd0RZR7Ku1ndXp5A@barnacle.rmq.cloudamqp.com/mtvezqic");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
                       //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null); // isso é pra funcionar independemente de quem rodar primeiro, (emissor ou receptor)
 
     String message = "Olá!!!";
     
                     //  (exchange, routingKey, props, message-body             ); 
-    channel.basicPublish("",       QUEUE_NAME, null,  message.getBytes("UTF-8"));
+    channel.basicPublish("",       QUEUE_NAME, null,  message.getBytes("UTF-8")); // aqui n estamos passando exchange, mas estamos passando a chave de roteamento padrão
     System.out.println(" [x] Mensagem enviada: '" + message + "'");
 
     channel.close();
--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
@@ -1,6 +1,6 @@
 package br.ufs.dcomp.ExemploRabbitMQ;
 
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.*; // API que implementa o protocolo AMQP
 
 import java.io.IOException;
 
@@ -9,18 +9,19 @@ public class Receptor {
   private final static String QUEUE_NAME = "minha-fila";
 
   public static void main(String[] argv) throws Exception {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    ConnectionFactory factory = new ConnectionFactory(); // pode fazer quantas conexoes quiser
+    factory.setUri("amqp://mtvezqic:oT0QoH-plEwlIWdYPd0RZR7Ku1ndXp5A@barnacle.rmq.cloudamqp.com/mtvezqic");
+    Connection connection = factory.newConnection(); // ele pode criar canais dentro de uma conexão TCP, independentes de ida e volta
+    Channel channel = connection.createChannel(); // abrindo a conexão
 
                       //(queue-name, durable, exclusive, auto-delete, params); 
     channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
     
     System.out.println(" [*] Esperando recebimento de mensagens...");
-
+    
+    /* metodo baseado em eventos*/
     Consumer consumer = new DefaultConsumer(channel) {
-      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException { // esse metodo é executado toda vez que uma mensagem chega pra uma fila
 
         String message = new String(body, "UTF-8");
         System.out.println(" [x] Mensagem recebida: '" + message + "'");
